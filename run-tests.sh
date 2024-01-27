ba16e6878d12ca3b59654772a52f8adc30c75ba4
Adrian Reber <adrian@lisas.de>
2024-01-27 17:54:06 +0100

    4
    Signed-off-by: Adrian Reber <adrian@lisas.de>

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,5 @@
 set +ex
 
 git clone https://github.com/adrianreber/ohpc-infrastructure.git
-cd ohpc-infrastructure.git/ansible/roles/test/files/
+cd ohpc-infrastructure/ansible/roles/test/files/
 ./run-ci.sh almalinux9.2 3.1
